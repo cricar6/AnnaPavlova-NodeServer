601119fc79b20169247a525acb2636e4154c0202
Cristian <cristian6569@gmail.com>
2018-05-01 16:10:27 -0500

    Añadido scs y carpeta publica. Es posible continuar con la maquetación en hbs

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const express = require('express'),
     MongoClient = require('mongodb').MongoClient;
     //ObjectID = require('mongodb').ObjectID;
     
+var app = express(),
+    db;
+
+app.engine('hbs', engines.handlebars);
+app.set ('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+    if (err) throw err;
+
+    db = client.db('annapavlova');
+
+    app.listen(3000);
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+    
+});
