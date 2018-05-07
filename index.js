b9a81515489c54dea5e50ad446d3a46ebb7fae8f
Cristian Rodriguez Cardona <cristian6569@gmail.com>
2018-05-07 08:01:31 -0500

    :c

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
 MongoClient.connect('mongodb+srv://cluster0-wiwgu.mongodb.net/annapavlova', 
 {
     auth: {
-        user: 'cricar',
+        user: 'cristian6569@gmail.com',
         password: 'naitsirc_6569'
     }
 
